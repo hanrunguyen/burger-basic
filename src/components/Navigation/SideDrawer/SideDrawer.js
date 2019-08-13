b17f7a777981e95d0882c130caa2a24d86d4e5e0
Truc Nguyen <thanh-truc.nguyen@ekino.com>
2019-08-13 18:00:43 +0700

    feat: #181

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,14 +2,16 @@ import React from "react";
 import classes from "./SideDrawer.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import Aux from "../../../hoc/Aux";
+import Aux from "../../../hoc/Aux/Aux";
 import Backdrop from "../../UI/Backdrop/Backdrop";
 import classnames from "classnames";
 
 const SideDrawer = props => {
-  const className = classnames(classes.SideDrawer, classes.Close, {
-    [classes.Open]: props.open
+  const className = classnames(classes.SideDrawer, {
+    [classes.Open]: props.open,
+    [classes.Close]: props.closed
   });
+
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
